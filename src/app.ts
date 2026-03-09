3f6370c8325df8fe1c3e235f30f74a3f22872d02
gus-p3 <sinnamexd3432@gmail.com>
2026-03-08 23:58:38 -0600

    Fix: AHora si ya puede empezar a trabajar, con los cors

--- src/app.ts
+++ src/app.ts
@@ -41,8 +41,6 @@ app.use('*', (req, res) => {
 
 // IMPORTANTE: Railway necesita 0.0.0.0
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {  // Añadir '0.0.0.0'
-  console.log(`⭐ Servidor corriendo en puerto ${PORT}`);
-});
+app.listen(PORT, () => console.log(`⭐ Servidor corriendo en puerto ${PORT}`));
 
 export default app;
