b49388f28fa69e983c8f69dfd4aa91e0ae34722b
gus-p3 <sinnamexd3432@gmail.com>
2026-04-07 00:01:28 -0600

    Fix: Arregle lo de las rutas

--- src/routes/inventoryRoutes.ts
+++ src/routes/inventoryRoutes.ts
@@ -1,21 +1,24 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
-import { getInventory, addInventoryItem } from '../controllers/inventoryController';
+import { getInventory, addInventoryItem, updateItem, deleteItem } from '../controllers/inventoryController';
 import { protect } from '../middlewares/authMiddleware';
 import { validate } from '../middlewares/validate';
 
 const router = Router();
 
 router.get('/',      protect, getInventory);
-router.post('/items', 
+router.post('/', 
   protect, 
   [
     body('name').notEmpty().withMessage('El nombre es requerido').trim().escape(),
-    body('quantity').isNumeric().withMessage('La cantidad debe ser un número').isFloat({ min: 0.1 }).withMessage('La cantidad mínima es 0.1'),
+    body('quantity').isNumeric().withMessage('La cantidad debe ser un número').isFloat({ min: 0 }).withMessage('La cantidad mínima es 0'),
     body('unit').optional().isString().trim().escape()
   ],
   validate,
   addInventoryItem
 );
+
+router.put('/:id', protect, updateItem);
+router.delete('/:id', protect, deleteItem);
 
 export default router;
